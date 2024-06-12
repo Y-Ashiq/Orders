1c7df1042766bb8328430caa1f26559120d2234f
Youssef Alashiq <100365853+Y-Ashiq@users.noreply.github.com>
2024-06-12 12:07:05 +0300

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "assignment5",
+  database: " ",  //database name 
 });
 
 export default connection;
